accaf182ec09612172b6f1548562d4232ef06c2d
Renato Groffe <renato.groffe@yahoo.com.br>
2024-11-20 23:09:15 -0300

    Commit inicial
    Commit inicial

---
+++ src/ConsoleAppChatAI/AI/ChatClientFactory.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleAppChatAI.AI;
+
+public static class ChatClientFactory
+{
+    public static IChatClient CreateClient(IConfiguration configuration, string model) =>
+        new OllamaChatClient(configuration["Ollama:Endpoint"]!, model);
+}
---
+++ src/ConsoleAppChatAI/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleAppChatAI.AI;
+using ConsoleAppChatAI.Inputs;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var logger = new LoggerConfiguration()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Literate)
+    .CreateLogger();
+
+var ollamaModel = InputHelper.GetOllamaModel();
+
+using var chatClient = ChatClientFactory.CreateClient(configuration, ollamaModel);
+
+while (true)
+{
+    Console.WriteLine();
+    logger.Information("Pressione CTRL + C para encerrar a aplicacao...");
+    var request = InputHelper.GetRequest();
+
+    var responseStream = chatClient.CompleteStreamingAsync(request);
+    var oldColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine();
+    await foreach (var chunk in responseStream)
+    {
+        Console.Write(chunk);
+    }
+    Console.WriteLine();
+    Console.ForegroundColor = oldColor;
+}
